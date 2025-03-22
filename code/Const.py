697c7344526f98c517ad5c4b8d2eaf15796d0b27
jonas@jonas.com <jonassaldanhanunes@gmail.com>
2025-03-22 01:36:12 -0300

    Adicionado assets próprios!

--- code/Const.py
+++ code/Const.py
@@ -6,12 +6,15 @@ COLOR_WHITE = (255, 255, 255)
 #E
 ENTITY_SPEED =  {
     'Level1Bg0': 0,
-    'Level1Bg1': 1,
-    'Level1Bg2': 2,
-    'Level1Bg3': 3,
-    'Level1Bg4': 4,
-    'Level1Bg5': 5,
-    'Level1Bg6': 6,
+    'Level1Bg1': 0,
+    'Level1Bg2': 1,
+    'Level1Bg3': 2,
+    'Level1Bg4': 3,
+    'Level1Bg5': 4,
+    'Level1Bg6': 5,
+    'Level1Bg7': 6,
+    'Level1Bg8': 7,
+    'Level1Bg9': 8,
 }
 #M
 MENU_OPTION = ("NEW GAME 1P",
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -10,7 +10,7 @@ class EntityFactory:
     def get_entity(entity_name: str, position=(0,0)):
         match entity_name:
             case "Level1Bg": list_bg = []
-        for i in range(7):
+        for i in range(10):
             list_bg.append(Background(f"Level1Bg{i}", (0,0)))
             list_bg.append(Background(f"Level1Bg{i}", (WIN_WIDTH, 0)))
         return list_bg
